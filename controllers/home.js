efe837ded924e153efa70b388ab7d8a5696bf2f9
leolanese <laneseleo@gmail.com>
2022-11-08 16:34:06 +0000

    nodejs inc

--- controllers/home.js
+++ controllers/home.js
@@ -1,3 +1,8 @@
 exports.get_home = function(req, res, next){
     res.render( 'home', { title: 'Express' } )
 }
+
+exports.submit_lead = function(req, res, next){
+    console.log('submit_lead:', req.body.email)
+    res.redirect('/')
+}
--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,8 @@ let home = require('../controllers/home');
 // router.get('/', function(req, res, next) {
 //   res.render('index', { title: 'Express' });
 // });
-router.get('/', home.get_home);
+router.get('/', home.get_home)
+router.post('/', home.submit_lead)
+
 
 module.exports = router;
